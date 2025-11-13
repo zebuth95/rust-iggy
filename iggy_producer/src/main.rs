e7f116afd37aa253730d9aecf4921a2db1c467aa
jaes <jaes9511@gmail.com>
2025-11-12 23:33:55 -0500

    iggy working version 0.7.0

--- iggy_producer/src/main.rs
+++ iggy_producer/src/main.rs
@@ -1,31 +1,38 @@
-use iggy::client::{Client, MessageClient, StreamClient, TopicClient, UserClient};
-use iggy::clients::client::IggyClient;
-use iggy::compression::compression_algorithm::CompressionAlgorithm;
-use iggy::messages::send_messages::{Message, Partitioning};
-use iggy::users::defaults::{DEFAULT_ROOT_PASSWORD, DEFAULT_ROOT_USERNAME};
-use iggy::utils::expiry::IggyExpiry;
-use iggy::utils::topic_size::MaxTopicSize;
+use iggy::prelude::*;
+use std::env;
 use std::error::Error;
-
-use std::time::Duration;
-use tokio::time::sleep;
+use std::str::FromStr;
 use tracing::{info, warn};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{EnvFilter, Registry};
 
 const STREAM_ID: u32 = 1;
 const TOPIC_ID: u32 = 1;
 const PARTITION_ID: u32 = 1;
+const BATCHES_LIMIT: u32 = 5;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    tracing_subscriber::fmt::init();
-    let client = IggyClient::default();
+    Registry::default()
+        .with(tracing_subscriber::fmt::layer())
+        .with(EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("INFO")))
+        .init();
+
+    let client = IggyClientBuilder::new()
+        .with_tcp()
+        .with_server_address(get_tcp_server_addr())
+        .build()?;
+
+    // Or, instead of above lines, you can just use below code, which will create a Iggy
+    // TCP client with default config (default server address for TCP is 127.0.0.1:8090):
+    // let client = IggyClient::default();
     client.connect().await?;
     client
         .login_user(DEFAULT_ROOT_USERNAME, DEFAULT_ROOT_PASSWORD)
         .await?;
     init_system(&client).await;
-    produce_messages(&client).await?;
-    Ok(())
+    produce_messages(&client).await
 }
 
 async fn init_system(client: &IggyClient) {
@@ -52,25 +59,33 @@ async fn init_system(client: &IggyClient) {
     }
 }
 
-async fn produce_messages(client: &IggyClient) -> Result<(), Box<dyn Error>> {
-    let interval = Duration::from_millis(500);
+async fn produce_messages(client: &dyn Client) -> Result<(), Box<dyn Error>> {
+    let duration = IggyDuration::from_str("500ms")?;
+    let mut interval = tokio::time::interval(duration.get_duration());
     info!(
-        "Messages will be sent to stream: {}, topic: {}, partition: {} with interval {} ms.",
+        "Messages will be sent to stream: {}, topic: {}, partition: {} with interval {}.",
         STREAM_ID,
         TOPIC_ID,
         PARTITION_ID,
-        interval.as_millis()
+        duration.as_human_time_string()
     );
 
     let mut current_id = 0;
     let messages_per_batch = 10;
+    let mut sent_batches = 0;
     let partitioning = Partitioning::partition_id(PARTITION_ID);
     loop {
+        if sent_batches == BATCHES_LIMIT {
+            info!("Sent {sent_batches} batches of messages, exiting.");
+            return Ok(());
+        }
+
+        interval.tick().await;
         let mut messages = Vec::new();
         for _ in 0..messages_per_batch {
             current_id += 1;
             let payload = format!("message-{current_id}");
-            let message = Message::new(Some(0), payload.into(), None);
+            let message = IggyMessage::from_str(&payload)?;
             messages.push(message);
         }
         client
@@ -81,7 +96,36 @@ async fn produce_messages(client: &IggyClient) -> Result<(), Box<dyn Error>> {
                 &mut messages,
             )
             .await?;
+        sent_batches += 1;
         info!("Sent {messages_per_batch} message(s).");
-        sleep(interval).await;
+    }
+}
+
+fn get_tcp_server_addr() -> String {
+    let default_server_addr = "127.0.0.1:8090".to_string();
+    let argument_name = env::args().nth(1);
+    let tcp_server_addr = env::args().nth(2);
+
+    if argument_name.is_none() && tcp_server_addr.is_none() {
+        default_server_addr
+    } else {
+        let argument_name = argument_name.unwrap();
+        if argument_name != "--tcp-server-address" {
+            panic!(
+                "Invalid argument {}! Usage: {} --tcp-server-address <server-address>",
+                argument_name,
+                env::args().next().unwrap()
+            );
+        }
+        let tcp_server_addr = tcp_server_addr.unwrap();
+        if tcp_server_addr.parse::<std::net::SocketAddr>().is_err() {
+            panic!(
+                "Invalid server address {}! Usage: {} --tcp-server-address <server-address>",
+                tcp_server_addr,
+                env::args().next().unwrap()
+            );
+        }
+        info!("Using server address: {}", tcp_server_addr);
+        tcp_server_addr
     }
 }
